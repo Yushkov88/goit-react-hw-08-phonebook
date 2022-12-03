8e3f875cc2f7c5086a7003abca53ac455ab201ac
Yushkov Vitaliy <98348805+Yushkov88@users.noreply.github.com>
2022-12-03 09:19:19 +0200

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,61 @@
-import ContactList from 'components/ContactList/ContactList';
-import Filter from 'components/Filter/Filter';
-import ContactForm from 'components/ContactForm/ContactForm';
-import { useState } from 'react';
+import { lazy, Suspense } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import ButtonAppBar from 'components/AppBar/AppBar';
+import ProtectedRoute from 'components/PrivateRoute';
+import PublicRoute from 'components/PublicRoute';
+
+const HomePage = lazy(() =>
+  import('./pages/HomePage' /* webpackChunkName: "HomePage" */)
+);
+
+const LoginPage = lazy(() =>
+  import('./pages/LoginPage' /* webpackChunkName: "LoginPage" */)
+);
+
+const RegisterPage = lazy(() =>
+  import('./pages/RegisterPage' /* webpackChunkName: "RegisterPage" */)
+);
+
+const ContactsPage = lazy(() =>
+  import('./pages/ContactsPage' /* webpackChunkName: "ContactsPage" */)
+);
 
 export default function App() {
-  const [filter, setFilter] = useState('');
-
-  const handleChange = e => {
-    setFilter(e);
-  };
-
   return (
-    <div>
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <h2>Contacts</h2>
-      <Filter filter={filter} handleChange={handleChange} />
-      <ContactList filter={filter} />
-    </div>
+    <Suspense fallback={<ButtonAppBar />}>
+      <Routes>
+        <Route path="/Phonebook/" element={<ButtonAppBar />}>
+          <Route index element={<HomePage />} />
+          <Route
+            path="register"
+            element={
+              <PublicRoute>
+                <RegisterPage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="login"
+            element={
+              <PublicRoute>
+                <LoginPage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="contacts"
+            element={
+              <ProtectedRoute>
+                <ContactsPage />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+        <Route
+          path="*"
+          element={<Navigate to="/Phonebook/" replace={true} />}
+        />
+      </Routes>
+    </Suspense>
   );
 }
